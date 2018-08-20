10f05f124357c4e47f1b252bb7299e40da5b3e16
Chuck Rabb <chuck.rabb@gmail.com>
2018-08-20 15:53:44 -0400

    Stupid auto format messed up my indents

--- button_state.cpp
+++ button_state.cpp
@@ -9,12 +9,12 @@
 void
 button_state::update( button* btn )
 {
-  ( digitalRead( btn->pin() ) == HIGH )
-  ? this->button_pressed( btn )
-  : this->button_unpressed( btn )
+    ( digitalRead( btn->pin() ) == HIGH )
+        ? this->button_pressed( btn )
+        : this->button_unpressed( btn )
   ;
 
-  return;
+    return;
 }
 
 
@@ -22,25 +22,25 @@ button_state::update( button* btn )
 void
 button_state::switch_to_pressed( button* btn )
 {
-  btn->current_state( btn->pressed_state() );
+    btn->current_state( btn->pressed_state() );
 
-  return;
+    return;
 }
 
 void
 button_state::switch_to_unpressed( button* btn )
 {
-  btn->current_state( btn->unpressed_state() );
+    btn->current_state( btn->unpressed_state() );
 
-  return;
+    return;
 }
 
 void
 button_state::switch_to_latched( button* btn )
 {
-  btn->current_state( btn->latched_state() );
+    btn->current_state( btn->latched_state() );
 
-  return;
+    return;
 }
 
 /* -------------------- UNPUSHED -------------------- */
@@ -48,16 +48,16 @@ button_state::switch_to_latched( button* btn )
 void
 button_state_unpressed::button_pressed( button* btn )
 {
-  this->switch_to_pressed( btn );
-  return;
+    this->switch_to_pressed( btn );
+    return;
 }
 
 void
 button_state_unpressed::button_unpressed( button* btn )
 {
-  // I'm already unpresed
-  //
-  return;
+    // I'm already unpresed
+    //
+    return;
 }
 
 /* -------------------- PUSHED -------------------- */
@@ -69,35 +69,35 @@ button_state_pressed::button_state_pressed()
 unsigned long
 button_state_pressed::mills_since_first_pressed()
 {
-  unsigned long now = millis();
-  unsigned long first_pressed = this->first_pressed_mills();
-  unsigned long interval = now - first_pressed;
+    unsigned long now = millis();
+    unsigned long first_pressed = this->first_pressed_mills();
+    unsigned long interval = now - first_pressed;
 
-  return interval;
+    return interval;
 }
 
 void
 button_state_pressed::button_unpressed( button* btn )
 {
-  this->switch_to_unpressed( btn );
-  return;
+    this->switch_to_unpressed( btn );
+    return;
 }
 
 void
 button_state_pressed::reset_state()
 {
-  button_state::reset_state();
-  this->first_pressed_mills( millis() );
-  this->has_updated_setpoint( false );
+    button_state::reset_state();
+    this->first_pressed_mills( millis() );
+    this->has_updated_setpoint( false );
 
-  return;
+    return;
 }
 
 bool
 button_state_pressed::should_latch()
 {
-  unsigned long interval = this->mills_since_first_pressed();
-  bool should_latch = ( interval > BTN_LATCHED_MILLS );
+    unsigned long interval = this->mills_since_first_pressed();
+    bool should_latch = ( interval > BTN_LATCHED_MILLS );
 
   return should_latch;
 }
@@ -105,24 +105,24 @@ button_state_pressed::should_latch()
 bool
 button_state_pressed::is_first_pressed()
 {
-  return !( this->has_updated_setpoint() );
+    return !( this->has_updated_setpoint() );
 }
 
 void
 button_state_pressed::button_pressed( button* btn )
 {
-  if ( this->is_first_pressed() )
-  {
-    btn->update_setpoint();
-    this->has_updated_setpoint( true );
-  }
-  else
-  {
-    if ( this->should_latch() )
+    if ( this->is_first_pressed() )
     {
-      this->switch_to_latched( btn );
+        btn->update_setpoint();
+        this->has_updated_setpoint( true );
     }
-  }
+    else
+    {
+        if ( this->should_latch() )
+        {
+            this->switch_to_latched( btn );
+        }
+    }
 
   return;
 }
@@ -134,20 +134,20 @@ button_state_pressed::button_pressed( button* btn )
 void
 button_state_latched::button_pressed( button* btn )
 {
-  if ( ( millis() - this->last_sp_change_mills() ) > BTN_UPDATE_SP_DELAY  )
-  {
-    btn->update_setpoint();
-    this->last_sp_change_mills( millis() );
-  }
+    if ( ( millis() - this->last_sp_change_mills() ) > BTN_UPDATE_SP_DELAY  )
+    {
+        btn->update_setpoint();
+        this->last_sp_change_mills( millis() );
+    }
 
-  return;
+    return;
 }
 
 void
 button_state_latched::reset_state()
 {
-  button_state_pressed::reset_state();
-  this->last_sp_change_mills( 0 );
+    button_state_pressed::reset_state();
+    this->last_sp_change_mills( 0 );
 }
 
 
